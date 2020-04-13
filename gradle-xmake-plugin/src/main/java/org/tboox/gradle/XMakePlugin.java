0fbd8cbfc30afca974be2ad81732c0f2a3328501
ruki <waruqi@gmail.com>
2020-04-13 22:42:22 +0800

    add gradle-xmake-plugin

---
+++ gradle-xmake-plugin/src/main/java/org/tboox/gradle/XMakePlugin.java
@@ -0,0 +1,14 @@
+package org.tboox.gradle;
+
+import org.gradle.api.*;
+import org.gradle.api.logging.LogLevel;
+import org.gradle.api.provider.Property;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.TaskContainer;
+
+public class XMakePlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+    }
+}
