2109b80a0a9766c60e0ea7ea48d932aa47165d73
ruki <waruqi@gmail.com>
2020-04-16 22:48:10 +0800

    add libs

--- nativelib/src/main/java/org/tboox/xmake/nativelib/Test.java
+++ nativelib/src/main/java/org/tboox/xmake/nativelib/Test.java
@@ -3,7 +3,7 @@ package org.tboox.xmake.nativelib;
 public class Test {
 
     public static String loadTests() {
-        System.loadLibrary("c++_shared");
+        //System.loadLibrary("c++_shared");
         System.loadLibrary("nativelib");
         return getNativeInfo();
     }
