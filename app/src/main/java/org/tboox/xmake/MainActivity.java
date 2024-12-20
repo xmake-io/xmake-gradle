45b8df1246788cd57eacb52ba2039af6c01d12f8
wps <19317362@qq.com>
2024-12-20 11:34:56 +0800

    [upgrade] 1. openjdk-17 2. AGP 3. Gradle 4. errors fixed while building

--- app/src/main/java/org/tboox/xmake/MainActivity.java
+++ app/src/main/java/org/tboox/xmake/MainActivity.java
@@ -1,20 +1,20 @@
 package org.tboox.xmake;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
+
+import org.tboox.xmake.app.R;
 import org.tboox.xmake.nativelib.Test;
 
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);  // Ensure this matches the layout file name
         String content = Test.loadTests();
         if (content != null) {
-            TextView textView = (TextView) findViewById(R.id.content);
+            TextView textView = findViewById(R.id.content);
             textView.setText(content);
         }
     }
